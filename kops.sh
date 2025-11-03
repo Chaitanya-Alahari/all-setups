8a7eb12bb9810eeefe5257dcbd864f37a16bcce3
Krishna Chaitanya Alahari <97384739+Chaitanya-Alahari@users.noreply.github.com>
2025-11-03 07:38:41 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -6,8 +6,8 @@
 #! /bin/bash
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kops-linux-amd64 kubectl
-mv kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mv kubectl /usr/local/bin/
 mv kops-linux-amd64 /usr/local/bin/kops
 
 export KOPS_STATE_STORE=s3://krishna03.k8s.local
