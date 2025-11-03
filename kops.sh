92721f6b7c3da5c722dfc2a72c709fc3e8913955
Krishna Chaitanya Alahari <97384739+Chaitanya-Alahari@users.noreply.github.com>
2025-11-03 07:33:20 +0530

    Fix kops.sh script by removing aws configure

--- kops.sh
+++ kops.sh
@@ -4,7 +4,6 @@
 
 
 #! /bin/bash
-aws configure
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kops-linux-amd64 kubectl
