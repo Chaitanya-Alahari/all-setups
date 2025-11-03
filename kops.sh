09ccb4b8b59dd9acaf80610bdab8a0301ee25d5e
Krishna Chaitanya Alahari <97384739+Chaitanya-Alahari@users.noreply.github.com>
2025-11-03 07:31:35 +0530

    Fix node size volume parameter in kops command

--- kops.sh
+++ kops.sh
@@ -12,5 +12,5 @@ mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
 export KOPS_STATE_STORE=s3://krishna03.k8s.local
-kops create cluster --name chaitu.k8s.local --zones us-east-1a,us-east-1b --master-count=1 --master-size t2.medium --master-volume-size 25 --node-count=2 --node-size t2.medium --node-size-volume 25
+kops create cluster --name chaitu.k8s.local --zones us-east-1a,us-east-1b --master-count=1 --master-size t2.medium --master-volume-size 25 --node-count=2 --node-size t2.medium --node-volume-size 25
 kops update cluster --name chaitu.k8s.local --yes --admin
