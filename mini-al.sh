b7698699e08fed96c621dab8795cf46c35a0fede
Krishna Chaitanya Alahari <97384739+Chaitanya-Alahari@users.noreply.github.com>
2025-11-05 13:02:08 +0530

    Refactor mini-al.sh to install kubectl and minikube
    Updated installation commands for kubectl and minikube, removed sudo usage.

--- mini-al.sh
+++ mini-al.sh
@@ -2,15 +2,22 @@
 #export PATH=$PATH:/usr/local/bin
 #source .bashrc
 
+yum update -y
 yum install docker -y
 systemctl start docker
+systemctl enable docker
 systemctl status docker
+
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo mv kubectl /usr/local/bin/kubectl
-sudo chmod +x /usr/local/bin/kubectl
+mv kubectl /usr/local/bin/
+chmod +x /usr/local/bin/kubectl
+
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
-sudo yum install iptables -y
+install minikube-linux-amd64 /usr/local/bin/minikube
+
+yum install iptables -y
 yum install conntrack -y
+yum install socat ebtables -y
+
 minikube start --driver=docker --force
-minikube status 
+minikube status
